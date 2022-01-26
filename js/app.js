9bfc248ae12c4c9141103d67f12629328441b5b1
A7mad3bd <ahmadabd398@gmail.com>
2022-01-26 08:44:15 +0200

    still_working

--- js/app.js
+++ js/app.js
@@ -2,19 +2,18 @@
 
 
 
+let employeeForm = document.getElementById('employeeForm');
+let employeeSection = document.getElementById('employee');
+
+var s = document.getElementById("Department");
+var dep = s.value;
+
+var s1 = document.getElementById("Level");
+var lvl = s1.value;
 
 
 
-function Senior(min, max) {
-   return (Math.random() * (max - min + 1)) + min;
-}
 
-function Mid_Senior(min, max) {
-        return (Math.random() * (max - min + 1)) + min;
-  }
-     function Junior(min, max) {
-        return (Math.random() * (max - min + 1)) + min;
-     }
 
 
 function Person(Employee_ID,Department, Level, Image_URL,Salary){
@@ -27,48 +26,115 @@ function Person(Employee_ID,Department, Level, Image_URL,Salary){
     
 
     Person.prototype.render = function (){
-        document.write(` id =<p>${this.emp_id}</p>`)
-        document.write(`Name - > <p> ${this.dep}  </p>`)
-        document.write(`Department - > <p> ${this.dep}  </p>`)
-        document.write(` Level - ><p> ${this.lv}  </p>`)
-        document.write(`Salary -> <p> ${this.sal}  </p>`)
+
+
+      let p3 = document.createElement('p1');
+      div.appendChild(p3);
+      p2.textContent = `Salary : ${this.emp_id}` ;
+
+
+      let div = document.createElement('div');
+      employeeSection.appendChild(div);
+  
+      let h4 = document.createElement('h4');
+      div.appendChild(h4);
+      h4.textContent = this.name;
+  
+      let p = document.createElement('p');
+      div.appendChild(p);
+      if(Department.select=="Administration"){
+          p.textContent =`${this.name} is available Hot and Cold and it has the following ingredients : `
+      }else if(Department.select=="Marketing"){
+          p.textContent =`${this.name} is available Cold and it has the following ingredients : `
+      }else if(Department.select=="Development"){
+          p.textContent =`${this.name} is available Hot and it has the following ingredients : `
+      }
+   else if(Department.select=="Finance"){
+      p.textContent =`${this.name} is available Hot and it has the following ingredients : `
+  }
+    
+
+  let p1 = document.createElement('p1');
+      div.appendChild(p1);
+      if(dep=="Administration"){
+          p.textContent =`${this.name} is available Hot and Cold and it has the following ingredients : `
+      }else if(dep=="Marketing"){
+          p.textContent =`${this.name} is available Cold and it has the following ingredients : `
+      }else if(dep=="Development"){
+          p.textContent =`${this.name} is available Hot and it has the following ingredients : `
+      }
+   else if(dep=="Finance"){
+      p.textContent =`${this.name} is available Hot and it has the following ingredients : `
+  }
+      
+  
+      let img = document.createElement('img');
+      div.appendChild(img);
+      img.setAttribute('src',this.imagePath);
+      img.setAttribute('alt',this.name);
+  
+      let p2 = document.createElement('p');
+      div.appendChild(p2);
+      p2.textContent = `Salary : ${this.Salary}` ;
+
+     
+
+
+
+
+
 
        }
 
        document.write("<br>");
+
        document.write("<br>");
-     let x1=new  Person("1000","Ghazi Samer","Administration","Senior",Senior(1500, 2000));
-     x1.render();
+
+
+
+
+       function handelSubmit(event) {
+         event.preventDefault();
+         let newemployee = Person(Employee_ID,Department, Level, Image_URL,Salary);
+         let name = event.target.name.value;
+         let Department = event.target.Department.value;
+         let Level = event.target.Level.value;
+
+        
+        
+        
+         employee.calculatePrice();
+         employee.render();
+     }
+
+
+
+
+
+     document.write("<br>");
      document.write("<br>");
 
-     let x2=new  Person("1001","Lana Ali" ,"Finance","Senior",Senior(1500, 2000));
-     x2.render();
-     document.write("<br>");
+  
 
 
-     let x3=new  Person("1002","Tamara Ayoub","Marketing","Senior",Senior(1500, 2000));
-     x3.render();
-     document.write("<br>");
 
 
-     let x4=new  Person("1003","Safi Walid","Administration","Mid-Senior",Mid_Senior(1000, 1500));
-     x4.render();
-     document.write("<br>");
+       Person.prototype.calculatePrice = function() {
+         this.price = getRandomNumber(1000,9999);
+      }
+      
+      function Senior(min, max) {
+         return ((Math.random() * (max - min + 1)) + min)*.925;//100-7.5
+      }
+      
+      function Mid_Senior(min, max) {
+              return ((Math.random() * (max - min + 1)) + min)*.925;//100-7.5
+        }
+           function Junior(min, max) {
+              return ((Math.random() * (max - min + 1)) + min)*.925;//100-7.5
+           }
 
-
-     let x5=new  Person("1004","Omar Zaid","Development","Senior", Senior(1500, 2000));
-     x5.render();
-     document.write("<br>");
-
-
-     let x6=new  Person("1005","Rana Saleh","Development","Junior",        Junior(500, 1000)     );
-     x6.render();
-     document.write("<br>");
-
-
-     let x7=new  Person("1006","Hadi Ahmad","Finance","Mid-Senior",Mid_Senior(1000, 1500));
-     x7.render();
-
+       employeeForm.addEventListener('submit', handelSubmit);
 
      
 

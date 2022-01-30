792f81a61908a8661856eb2b6be90e06e68dad8a
A7mad3bd <ahmadabd398@gmail.com>
2022-01-30 04:12:33 +0200

    localStorage

--- js/app.js
+++ js/app.js
@@ -1,27 +1,22 @@
 'use strict';
-
-
-
+const tableCell = document.getElementById("tablecellid");
 let employeeForm = document.getElementById('employeeForm');
 let employeeSection = document.getElementById('employee');
 let div = document.createElement('div');
 employeeSection.appendChild(div);
+let a;
+let countsal=0;
+
+let employeeSection1 = document.getElementById('employee1');
+let div1 = document.createElement('div1');
+employeeSection.appendChild(div1);
+
+let Allemployees = [];
+checkLocalAndPush();
 
 
-
-//var s = document.getElementById("Department");
-//var dep = s.value;
-
-//var s1 = document.getElementById("Level");
-//var lvl = s1.value;
-
-
-
-
-
-
-function Person(Nameemp,Department, Level, image) {
-    this.na=Nameemp;
+function Person(Nameemp, Department, Level, image) {
+    this.na = Nameemp;
     this.dep = Department;
     this.lv = Level;
     this.img = image;
@@ -29,114 +24,76 @@ function Person(Nameemp,Department, Level, image) {
     this.sal = 0;
 }
 
-Person.prototype.addemp_id = function () {
-    this.emp_id = getRandomNumber(1000, 9999);
-}
+function render(employeesFromLS) {
+    employeeSection.innerHTML = '';
+    for (let i = 0; i < employeesFromLS.length; i++) {
 
-Person.prototype.calculatesalary = function () {
-    if (this.dep == "Senior") {
-        this.sal = getRandomNumber1(1000, 2000);
-    } else if (this.lv == "Mid-Senior") {
-        this.sal = getRandomNumber1(1000, 2000);
-    } else if (this.lv == "Junior") {
-        this.sal = getRandomNumber1(1000, 2000);
+        let image = document.createElement('img');
+        div.appendChild(image);
+        image.setAttribute('src', employeesFromLS[i].img);
+        
+        let h4 = document.createElement('h4');
+        div.appendChild(h4);
+        h4.textContent = employeesFromLS[i].na;
+
+        let h5 = document.createElement('h4');
+        div.appendChild(h5);
+        h5.textContent = employeesFromLS[i].dep;
+
+        let lv = document.createElement('h4');
+        div.appendChild(lv);
+        lv.textContent = employeesFromLS[i].lv;
+
+        let p2 = document.createElement('p');
+        div.appendChild(p2);
+        p2.textContent = `Employee id : ${employeesFromLS[i].emp_id}`;
+
+        let p3 = document.createElement('p');
+        div.appendChild(p3);
+        p3.textContent = `Salary : ${employeesFromLS[i].sal}`;
+
+
+
+///
+
+// Render each department information in a table:
+// Department Name
+// Number of employees in each department.
+// Average salary of the department
+
+// Total salary Where average salary is the summation of salaries for all employees in each department divided by the number of the employees in that department and total salary is the summation of salaries for all employees in each department.
+// Add a table footer which will have the total number of employees, the average salary for all departments and the total salary for all departments, as shown below.
+     
+
+        //  countsal+=employeesFromLS[i].sal;
+        //   let avg1=countsal/employeesFromLS.length;
+
+
+        // let totsal =tableCell.textContent = `${countsal}`;
+        // tableCell.innerHtml=totsal ;
+
+
+        // let avgsal =tableCell.textContent = `${avg1}`;
+        // tableCell.innerHtml=avgsal ;
+
+
+       
+
+
+
+
+        
+        
+
     }
 
+   
+    
+
 
 }
 
 
-
-
-
-Person.prototype.render = function () {
-   
-
-    // let nm = document.createElement('h4');
-    // div.appendChild(nm);
-    // nm.textContent = `Employee_Name : ${this.na}`;
-
-
-   
-
-    // let h4 = document.createElement('h4');
-    // div.appendChild(h4);
-    // h4.textContent = `Employee_Department : ${this.dep}`;
-
-
-    // let lv = document.createElement('h4');
-    // div.appendChild(lv);
-    // lv.textContent = `Employee_Level : ${this.lv}`;
-
-
-
-    // let p = document.createElement('p');
-    // div.appendChild(p);
-    // if (this.dep == "Administration") {
-    //     p.textContent = `1 `
-    // } else if (this.dep == "Marketing") {
-    //     p.textContent = `1 `
-    // } else if (this.dep == "Development") {
-    //     p.textContent = `1 `
-    // } else if (this.dep == "Finance") {
-    //     p.textContent = `1 `
-    // }
-
-
-    // let h22 = document.createElement('h4');
-    // div.appendChild(h22);
-    // h22.textContent = this.lv;
-
-
-
-    // if (this.lv == "Senior") {
-    //     h22 .textContent = `1 `
-    // } else if (this.lv == "Mid-Senior") {
-    //     h22.textContent = `1 `
-    // } else if (this.lv == "Junior") {
-    //     h22.textContent = `1 `
-    // }
-
-    
-
-   
-    let image = document.createElement('img');
-    div.appendChild(image);
-    
-    image.setAttribute('src', this.img);
-    image.setAttribute('alt', this.dept);
-    
-
-
-    let final = document.createElement('h4');
-    div.appendChild(final);
-    final.textContent = `Employee: ${this.na} -ID :${this.emp_id}   Deperatment : ${this.dep} - Level : ${this.lv}    ${this.sal} `;
-
-
-    // let p2 = document.createElement('p');
-    // div.appendChild(p2);
-    // p2.textContent = `Employee_id : ${this.emp_id}`;
-
-  
-    // let p3 = document.createElement('p');
-    // div.appendChild(p3);
-    // p3.textContent = `Salary : ${this.sal}`;
-
-
-
-
-    
-}
-
-
-
-
-
-
-
-
-
-document.write("<br>");
 
 document.write("<br>");
 
@@ -162,14 +119,76 @@ function handelSubmit(event) {
 
     newemployee.calculatesalary();
 
-    newemployee.render();
+    
+
+
+
+    Allemployees.push(newemployee);
+
+    let jsonArr = toJSON();
+
+    saveToLocalS(jsonArr);
+
+    render(readFromLocalS());
 }
 
 
 
 document.write("<br>");
+
+function checkLocalAndPush() {
+    if (Allemployees.length == 0) {
+        let arr = readFromLocalS();
+        if (arr.length != 0) {
+            Allemployees = arr;
+        }
+    }
+}
+
+
+
+
+function readFromLocalS() {
+    let jsonArr = localStorage.getItem('employees');
+    let arr = JSON.parse(jsonArr);
+    if (arr !== null) {
+        return arr;
+    } else {
+        return [];
+    }
+}
+
+
+function toJSON() {
+    let jsonArray = JSON.stringify(Allemployees);
+    return jsonArray;
+}
+
+
+function saveToLocalS(jsonArray) {
+    localStorage.setItem('employees', jsonArray)
+}
+
+
+
 document.write("<br>");
 
+Person.prototype.calculatesalary = function () {
+    if (this.dep == "Senior") {
+        this.sal = getRandomNumber1(1000, 2000);
+    } else if (this.lv == "Mid-Senior") {
+        this.sal = getRandomNumber1(1000, 2000);
+    } else if (this.lv == "Junior") {
+        this.sal = getRandomNumber1(1000, 2000);
+    }
+
+
+}
+
+
+Person.prototype.addemp_id = function () {
+    this.emp_id = getRandomNumber(1000, 9999);
+}
 
 function getRandomNumber1(min, max) {
     return (Math.floor(Math.random() * (max - min) + min)) * .925;
@@ -179,8 +198,12 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+render(readFromLocalS());
+
 
 employeeForm.addEventListener('submit', handelSubmit);
+employeeForm.addEventListener('submit1', handelSubmit);
+
 
 
 
